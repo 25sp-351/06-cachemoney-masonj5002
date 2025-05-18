8c2fda2627f072ce3fb353abfa5e8d67d7538940
Mason Jennings <masonj@csu.fullerton.edu>
2025-05-17 21:10:01 -0700

    moved functions in cache_money to below main()

--- cache_money.c
+++ cache_money.c
@@ -9,33 +9,27 @@
 
 typedef char* (*char_ptr_func_ptr)(int param);
 char_ptr_func_ptr dollarsToString_provider;
-
-char* intToString_adapter(int value) {
-    printf("ADAPTER CALLED for %d\n", value);
-    return intToString(value);
-}
-
 ProviderFunction cached_provider = NULL;
 
-char* cached_wrapper(int value) {
-    return (*cached_provider)(value);
-}
+char* intToString_adapter(int value);
+char* cached_wrapper(int value);
+
 
 int main(int argc, char** argv) {
     const char* cache_policy = "./random_replacement.so";
-
+    
     if (argc > 1)
-        cache_policy = argv[1];
-
+    cache_policy = argv[1];
+    
     Cache* cache_hooks = load_cache_module(cache_policy);
     if (!cache_hooks) {
         fprintf(stderr, "Failed to load cache policy: %s\n", cache_policy);
         return 1;
     }
-
+    
     cached_provider = cache_hooks->set_provider_func(intToString_adapter);
     dollarsToString_provider = cached_wrapper;
-
+    
     printf("%s\n\n", (*dollarsToString_provider)(0));
     printf("%s\n\n", (*dollarsToString_provider)(22));
     printf("%s\n\n", (*dollarsToString_provider)(1500));
@@ -48,11 +42,20 @@ int main(int argc, char** argv) {
     printf("%s\n\n", (*dollarsToString_provider)(1024));
     printf("%s\n\n", (*dollarsToString_provider)(1025));
     printf("%s\n\n", (*dollarsToString_provider)(1026));
-
+    
     print_cache_stats(STDOUT_FILENO, cache_hooks->get_statistics());
-
+    
     cache_hooks->cache_cleanup();
     free(cache_hooks);
-
+    
     return 0;
+}
+
+char* intToString_adapter(int value) {
+    printf("ADAPTER CALLED for %d\n", value);
+    return intToString(value);
+}
+
+char* cached_wrapper(int value) {
+    return (*cached_provider)(value);
 }
